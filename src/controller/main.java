ee5a11fdb61046a4075069ecad219a50a4471166
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2020-08-03 15:03:31 +0200

    creation du main et lancement

---
+++ src/controller/main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author gauthierbohyn
+ *
+ */
+import view.start;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		start view = new view.start();
+		view.main(args);
+		
+	}
+
+}
