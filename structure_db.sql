8a6c4a91213a80687567cf6152d23a4fdfc0021b
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2020-08-10 20:15:57 +0200

    update structure de la db (voir dans le futur)

--- structure_db.sql
+++ structure_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 07, 2020 at 05:38 PM
+-- Generation Time: Aug 10, 2020 at 08:15 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -29,6 +29,33 @@ CREATE TABLE `clients` (
   `telephone` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `descriptions`
+--
+
+CREATE TABLE `descriptions` (
+  `id` int(11) NOT NULL,
+  `quantite` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `prixUnitaire` int(11) DEFAULT NULL,
+  `factureId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `factures`
+--
+
+CREATE TABLE `factures` (
+  `id` int(11) NOT NULL,
+  `montantTVA` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `clientId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -40,6 +67,20 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `descriptions`
+--
+ALTER TABLE `descriptions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `factureId` (`factureId`);
+
+--
+-- Indexes for table `factures`
+--
+ALTER TABLE `factures`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `clientId` (`clientId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -48,3 +89,31 @@ ALTER TABLE `clients`
 --
 ALTER TABLE `clients`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `descriptions`
+--
+ALTER TABLE `descriptions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `factures`
+--
+ALTER TABLE `factures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `descriptions`
+--
+ALTER TABLE `descriptions`
+  ADD CONSTRAINT `descriptions_ibfk_1` FOREIGN KEY (`factureId`) REFERENCES `factures` (`id`);
+
+--
+-- Constraints for table `factures`
+--
+ALTER TABLE `factures`
+  ADD CONSTRAINT `factures_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `clients` (`id`);
